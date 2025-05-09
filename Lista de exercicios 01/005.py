922a3c07e0eb3b3e52b2cb7508b81d6cb118d61d
Luciano Simas <luciano.simasjr@gmail.com>
2025-05-09 14:22:30 -0300

    Update 005.py

--- Lista de exercicios 01/005.py
+++ Lista de exercicios 01/005.py
@@ -6,10 +6,18 @@
 num1 = int(input('Me informe o primeiro número:  '))
 num2 = int(input('Me informe o segundo número:  '))
 
-soma = num1 + num2
-produto = num1 * num2 
-quociente = num1 / num2 
 
-print(f"A soma dos dois números informados é: {soma}")
-print(f"O produto dos dois números informados é: {produto}")
-print(f"O quociente dos dois números informados é: {quociente:.1f}")
+if num2 == 0:
+    print(f'{num1} / {num2}; Não é possivel gerar o resultado do quociente.')
+    soma = num1 + num2
+    produto = num1 * num2 
+    print(f"A soma dos dois números informados é: {soma}")
+    print(f"O produto dos dois números informados é: {produto}")
+
+else: 
+    soma = num1 + num2
+    produto = num1 * num2 
+    quociente = num1 * num2
+    print(f"A soma dos dois números informados é: {soma}")
+    print(f"O produto dos dois números informados é: {produto}")
+    print(f"O quociente dos dois números informados é: {quociente:.1f}")
